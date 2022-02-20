edb59a673171e6332504a8b12ce6fe3bf7349c5e
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2022-02-20 02:03:40 -0500

    logo and header added

---
+++ my-app/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA-XnZhCp5aO6bjHYp1XmT0Bx_WgwSHXow",
+  authDomain: "challenge-e932f.firebaseapp.com",
+  projectId: "challenge-e932f",
+  storageBucket: "challenge-e932f.appspot.com",
+  messagingSenderId: "679485018389",
+  appId: "1:679485018389:web:d220144b346b844555591c",
+  measurementId: "G-DLXMLFKS7S"
+};
