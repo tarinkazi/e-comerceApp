8eb4ac839557401945e8ace14380e3a5d0f84b2d
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2022-02-21 03:36:16 -0500

    router dom setup

---
+++ my-app/src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function About(){
+  return(
+    <div>
+      I am at Checkout
+    </div>
+  )
+}
+
+export default About;
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,16 +2,33 @@ import React from 'react';
 import './App.css';
 import Header from './Header.js';
 import Home from './Home';
+import About from './About'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      
-        <Header/>
 
-         <Home />
-     
-    </div>
+    <Router>
+      <div className='App'>
+        <Header/>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+        </Routes>
+
+        <Routes>
+          <Route exact path="/checkout" element={<About/>} />
+            
+         
+        </Routes>
+
+      </div>
+    </Router>
+    
+    
+  
+
   );
 }
 
