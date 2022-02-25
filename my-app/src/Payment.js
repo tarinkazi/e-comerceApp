86b1d6de035df6eaf4fafb7a75145342b866e6ff
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2022-02-25 04:00:13 -0500

    payment method design added

--- my-app/src/Payment.js
+++ my-app/src/Payment.js
@@ -2,6 +2,8 @@ import React from "react";
 import './Payment.css';
 import {useStateValue} from './StateProvider';
 import CheckoutProduct from "./ChectoutProduct";
+import Checkout from './Checkout';
+import {Link} from 'react-router-dom';
 
 function Payment(){
   const [{basket, user}, dispatch] =useStateValue();
@@ -9,6 +11,9 @@ function Payment(){
   return(
     <div className="payment">
       <div className="payment__container">
+        <h1>
+          Checkout(<Link to='/checkout'>{basket?.length} items</Link>)
+        </h1>
         <div className="payment__section">
           <div className="payment__title">
                 <h3>Delivery add</h3>
@@ -34,6 +39,15 @@ function Payment(){
               rating={item.rating}
             />
           ))}
+          </div>
+        </div>
+
+        <div className="payment__section">
+          <div className="payment__title">
+              <h3>payment__method</h3>
+          </div>
+          <div className="payment__details">
+
           </div>
         </div>
 
