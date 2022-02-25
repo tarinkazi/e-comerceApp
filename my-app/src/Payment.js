7a21fc115dc3c7fa4b635207bec77c6ed6a63a22
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2022-02-25 03:36:33 -0500

    payment method design

--- my-app/src/Payment.js
+++ my-app/src/Payment.js
@@ -1,14 +1,43 @@
 import React from "react";
 import './Payment.css';
+import {useStateValue} from './StateProvider';
+import CheckoutProduct from "./ChectoutProduct";
+
 function Payment(){
+  const [{basket, user}, dispatch] =useStateValue();
+ 
   return(
     <div className="payment">
       <div className="payment__container">
         <div className="payment__section">
           <div className="payment__title">
-
+                <h3>Delivery add</h3>
+          </div>
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>123 bahadur pur</p>
+            <p>Toronto</p>
           </div>
         </div>
+
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review Items and Delivery</h3>
+          </div>
+          <div className="payment__items">
+          {basket.map(item => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
+          </div>
+        </div>
+
+
 
       </div>
       
