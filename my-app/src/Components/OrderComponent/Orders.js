589878d2c0ae0784aaf73ad8f3998c8a972840c0
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2022-02-27 14:49:27 -0500

    order page added

--- my-app/src/Components/OrderComponent/Orders.js
+++ my-app/src/Components/OrderComponent/Orders.js
@@ -1,13 +1,43 @@
-import React from "react";
-import "./Orders.css"
+import React, { useState, useEffect } from 'react';
+import { db } from "../../firebase";
+import './Orders.css'
+import { useStateValue } from "../../Helper/StateProvider";
+import Order from './Order'
 
+function Orders() {
+  const [{ basket, user }, dispatch] = useStateValue();
+  const [orders, setOrders] = useState([]);
 
-function Orders(){
-  return(
-    <div>
-      I am at Orders
-    </div>
-  )
+  useEffect(() => {
+    if(user) {
+        db
+        .collection('users')
+        .doc(user?.uid)
+        .collection('orders')
+        .orderBy('created', 'desc')
+        .onSnapshot(snapshot => (
+            setOrders(snapshot.docs.map(doc => ({
+                id: doc.id,
+                data: doc.data()
+            })))
+        ))
+    } else {
+        setOrders([])
+    }
+
+  }, [user])
+
+    return (
+        <div className='orders'>
+            <h1>Your Orders</h1>
+
+            <div className='orders__order'>
+                {orders?.map(order => (
+                    <Order order={order} />
+                ))}
+            </div>
+        </div>
+    )
 }
 
-export default Orders;
+export default Orders
